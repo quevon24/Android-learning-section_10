4cf476129381e8057d78d5683934b4acaee24c03
Kevin ramirez <kevin.ramirez@jbge.com.mx>
2017-12-05 16:36:38 -0600

    traducciones v1.0

--- app/src/main/java/com/seccion10/seccion_10/MainActivity.java
+++ app/src/main/java/com/seccion10/seccion_10/MainActivity.java
@@ -2,6 +2,7 @@ package com.seccion10.seccion_10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // traducciones
+        String welcome = getString(R.string.welcome);
+        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/seccion10/seccion_10/Util.java
@@ -0,0 +1,15 @@
+package com.seccion10.seccion_10;
+
+import android.content.Context;
+
+/**
+ * Created by quevon24 on 05/12/2017.
+ */
+
+public class Util {
+
+    public static String getWelcome(Context context) {
+        // traduccion en clases
+        return context.getString(R.string.welcome);
+    }
+}
